6f4f52965781d3ef3f80aad26bc9e4a49bb29ef5
Storm-Breaker <pedrojosejr13@gmail.com>
2019-09-18 23:38:59 -0300

    Improvements on receiver

--- game-of-three-player/src/main/java/com/challenge/gameofthreeplayer/event/receiver/impl/PlayerReceiver.java
+++ game-of-three-player/src/main/java/com/challenge/gameofthreeplayer/event/receiver/impl/PlayerReceiver.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
+import static java.lang.String.*;
+
 public class PlayerReceiver extends JSONReceiver<GameEvent> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PlayerReceiver.class);
@@ -31,13 +33,31 @@ public class PlayerReceiver extends JSONReceiver<GameEvent> {
         GameMoveDTO move = performMove(event.getCurrentScore());
         move.setPlayer(event.getPlayer());
 
-        LOGGER.info(String.format("GAME_INFO - Game: %d - Player_%d;current_score:%d;added_score:%s;final_score:%d;",
+        printStatus(move, event);
+        sendNewMove(event.getGameId(), move);
+
+    }
+
+    private void sendNewMove(Long gameId, GameMoveDTO move) {
+        try {
+            ResponseEntity<?> response = gameApiClient.doMove(gameId, move);
+
+            if (response.getStatusCode() == HttpStatus.OK) {
+                LOGGER.info(format("M=process;state=info;message=Move request send successfully to the server;details=%s;", response.getStatusCode().toString()));
+            }
+        }catch(Exception ex) {
+            LOGGER.error("M=process;state=error;message=Move cloud not be sent;cause=%s;", ex.getMessage());
+        }
+    }
+
+    private void printStatus(GameMoveDTO move, GameEvent event) {
+        LOGGER.info(format("GAME_INFO - Game: %d - Player_%d;current_score:%d;added_score:%s;final_score:%d;",
                 event.getGameId(), move.getPlayer(), move.getOriginalScore(), move.getAddedScore(), move.getFinalScore()));
 
-        ResponseEntity<?> response = gameApiClient.doMove(event.getGameId(), move);
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            LOGGER.info("M=process;state=info;details=%s;message=Move request send successfully to the server", response.getStatusCode().toString());
+        if (move.getFinalScore() == 1) {
+            LOGGER.info("YOU WIN!! CONGRATS!!");
+        }else if ( move.getFinalScore() < 3 ) {
+            LOGGER.info("THIS GAME HAS NO WINNER.");
         }
     }
 
@@ -47,7 +67,7 @@ public class PlayerReceiver extends JSONReceiver<GameEvent> {
         if( auto ) {
             addedScore = getAutoAddedScore(originalScore);
         }else{
-            addedScore = getManualAddedScore();
+            addedScore = getManualAddedScore(originalScore);
         }
 
         Integer finalScore = (originalScore + addedScore) / 3;
@@ -60,12 +80,12 @@ public class PlayerReceiver extends JSONReceiver<GameEvent> {
                 .findFirst().orElse(-1);
     }
 
-    private Integer getManualAddedScore() {
+    private Integer getManualAddedScore(Integer originalScore) {
         Integer addedValue;
 
         do {
             Scanner keyboard = new Scanner(System.in);
-            System.out.println("Add a score of 1, 0 or -1 to the current score: ");
+            System.out.println(String.format("Add a score of 1, 0 or -1 to the (current score is %d): ", originalScore));
             addedValue = keyboard.nextInt();
 
         }while ( addedValue > 1 || addedValue < -1 );
